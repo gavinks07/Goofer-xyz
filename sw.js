6c56198f8beb160edc6cd5b91dd6fd69f3b21b3f
gavinks07 <71905804+gavinks07@users.noreply.github.com>
2023-07-10 21:45:03 -0600

    wsaf

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener("install", e => {
     e.waitsUntil(
         caches.open("static").then(cache => {
             return cache.addAkk(["./", "./index.html", "icon-192x192.png", "script.js"]);
+
         })
     );
 });
-
