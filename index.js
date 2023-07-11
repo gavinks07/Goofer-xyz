fbbc714d72bae72cb288695f167e03507a80c816
gavinks07 <71905804+gavinks07@users.noreply.github.com>
2023-07-10 21:50:48 -0600

    hi

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(Registration => {
+    navigator.serviceWorker.register("sw.js").then(registration => {
         console.log("SW Registered!");
-        console.log(Registration);
+        console.log(registration);
     }).catch(error => {
-        console.log("SW registration Failed!");
+        console.log("SW registration failed!");
         console.log(error);
     });
 }
