f8237739e47f8db8af06b2569bf76d1f893b0d47
gavinks07 <71905804+gavinks07@users.noreply.github.com>
2023-07-11 16:08:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,61 @@ function decryptText(encryptedText, key) {
   }
   return result;
 }
+
+// Add a click event listener to the document to close the copy button popup when clicked outside
+document.addEventListener('click', function (event) {
+  if (!event.target.closest('.copy-button') && !event.target.closest('.copy-popup-content')) {
+    hideCopyOptions();
+  }
+});
+
+// Function to show the copy button popup
+function showCopyOptions() {
+  document.getElementById('copyPopupOverlay').style.display = 'flex';
+}
+
+// Function to hide the copy button popup
+function hideCopyOptions() {
+  document.getElementById('copyPopupOverlay').style.display = 'none';
+}
+
+// Function to toggle the selected options
+function toggleOption(option) {
+  const checkbox = document.getElementById(`option-${option}`);
+  checkbox.checked = !checkbox.checked;
+}
+
+// Function to copy the selected options
+function copySelectedOptions() {
+  const link = 'goofer.xyz/';
+  const key = document.getElementById('key-input').value;
+  const output = document.getElementById('output').value;
+
+  const selectedOptions = [];
+  const options = document.querySelectorAll('.copy-popup-content .option input[type="checkbox"]');
+  options.forEach((option) => {
+    if (option.checked) {
+      selectedOptions.push(option.id.split('-')[1]);
+    }
+  });
+
+  let textToCopy = '';
+  selectedOptions.forEach((option) => {
+    if (option === 'link') {
+      textToCopy += link + '\n';
+    } else if (option === 'key') {
+      textToCopy += key + '\n';
+    } else if (option === 'output') {
+      textToCopy += output + '\n';
+    }
+  });
+
+  navigator.clipboard.writeText(textToCopy.trim())
+    .then(() => {
+      alert('Copied:\n' + textToCopy);
+      hideCopyOptions();
+    })
+    .catch((error) => {
+      console.error('Error copying text:', error);
+    });
+}
