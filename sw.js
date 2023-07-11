1afd9d5e3f4a7f8918cb9dcb55e445b35b07f303
gavinks07 <71905804+gavinks07@users.noreply.github.com>
2023-07-10 21:47:33 -0600

    hi

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 self.addEventListener("install", e => {
-    e.waitsUntil(
+    e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAkk(["./", "./index.html", "icon-192x192.png", "script.js"]);
-
+            return cache.addAll(["./", "./index.html", "icon-192x192.png", "script.js"]);
         })
     );
 });
